211351f1ef336771901d4b0303de2991cea2b3c8
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-10-06 10:38:56 -0400

    player moves, working on making bullets

--- try2/Ship.js
+++ try2/Ship.js
@@ -5,16 +5,28 @@ export default class Ship {
     this.el = document.createElement("img");
     this.el.src = SHIP_IMAGE;
     document.body.append(this.el);
+
     this.el.className = "ship";
+
     this.setX(window.innerWidth / 2);
     this.setY(window.innerHeight - 80);
+    this.SPEED = 4;
+    this.SHIP_IMAGE_WIDTH = 50;
   }
   setX(x) {
     this.x = x;
-    this.el.style.left = ` ${this.x}px`;
+    this.el.style.left = `${this.x}px`;
   }
   setY(y) {
     this.y = y;
     this.el.style.top = `${this.y}px`;
+  }
+  moveRight() {
+    console.log("right");
+    this.setX(this.x + this.SPEED);
+  }
+  moveLeft() {
+    console.log("left");
+    this.setX(this.x - this.SPEED);
   }
 }
--- try2/app.js
+++ try2/app.js
@@ -1,3 +1,32 @@
 import Ship from "./Ship.js";
+import Bullet from "./Bullet.js";
+const keys = {
+  a: false,
+  d: false,
+  [" "]: false,
+};
 
-new Ship();
+document.addEventListener("keydown", (e) => {
+  keys[e.key] = true;
+  console.log(keys.a);
+});
+
+document.addEventListener("keyup", (e) => {
+  keys[e.key] = false;
+});
+
+const ship = new Ship();
+
+const update = () => {
+  if (keys.a && ship.x > 0) {
+    ship.moveLeft();
+  }
+  if (keys.d && ship.x < window.innerWidth - ship.SHIP_IMAGE_WIDTH) {
+    ship.moveRight();
+  }
+  if (keys[" "]) {
+    // create a bullet
+  }
+};
+
+setInterval(update, 20);

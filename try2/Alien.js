bc683cb647c1544fe811bcb4bcccd1712bb82a59
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-10-07 10:38:32 -0400

    alien movement done, working on collision of bullets and aliens

--- try2/Alien.js
+++ try2/Alien.js
@@ -1,38 +1,34 @@
 const AlIEN_IMAGE = "./imgs/ufo.png";
 import Entity from "./Entity.js";
 
+const LEFT = "left";
+const RIGHT = "right";
+
 export default class Alien extends Entity {
   constructor({ x, y }) {
     super({ tag: "img", className: "alien" });
     this.el.src = AlIEN_IMAGE;
-    // document.body.append(this.el);
-
-    // this.SPEED = 8;
-    // this.SHIP_IMAGE_WIDTH = 50;
-    // this.canFire = true;
-
+    this.direction = LEFT;
+    this.SPEED = 2;
+    this.DISTANCE = 40;
     this.setX(x);
     this.setY(y);
   }
-  // moveRight() {
-  //   console.log("right");
-  //   this.setX(this.x + this.SPEED);
-  // }
-  // moveLeft() {
-  //   console.log("left");
-  //   this.setX(this.x - this.SPEED);
-  // }
-
-  // fire({ createBullet }) {
-  //   if (this.canFire) {
-  //     this.canFire = false;
-  //     createBullet({
-  //       x: this.x,
-  //       y: this.y,
-  //     });
-  //     setTimeout(() => {
-  //       this.canFire = true;
-  //     }, 150);
-  //   }
-  // }
+  setDirectionLeft() {
+    this.direction = LEFT;
+  }
+  setDirectionRight() {
+    this.direction = RIGHT;
+  }
+  moveDown() {
+    this.setY(this.y + this.DISTANCE);
+  }
+  update() {
+    if (this.direction === LEFT) {
+      this.setX(this.x - this.SPEED);
+      // this.moveDown();
+    } else {
+      this.setX(this.x + this.SPEED);
+    }
+  }
 }
--- try2/app.js
+++ try2/app.js
@@ -30,8 +30,21 @@ for (let row = 0; row < 2; row++) {
       x: col * 100 + 200,
       y: row * 100 + 40,
     });
+    aliens.push(alien);
   }
 }
+
+const getLeftMostAlien = () => {
+  return aliens.reduce((minimumAlien, currentAlien) => {
+    return currentAlien.x < minimumAlien.x ? currentAlien : minimumAlien;
+  });
+};
+
+const getRightMostAlien = () => {
+  return aliens.reduce((maximumAlien, currentAlien) => {
+    return currentAlien.x > maximumAlien.x ? currentAlien : maximumAlien;
+  });
+};
 
 const createBullet = ({ x, y }) => {
   bullets.push(
@@ -63,8 +76,26 @@ const update = () => {
       bullet.remove();
       bullets.splice(bullets.indexOf(bullet), 1);
     }
-    console.log(bullets);
+    // console.log(bullets);
   });
+
+  aliens.forEach((alien) => {
+    alien.update();
+  });
+
+  if (getLeftMostAlien().x < 100) {
+    aliens.forEach((alien) => {
+      alien.moveDown();
+      alien.setDirectionRight();
+    });
+  }
+  if (getRightMostAlien().x > window.innerWidth - 100) {
+    aliens.forEach((alien) => {
+      alien.moveDown();
+
+      alien.setDirectionLeft();
+    });
+  }
 };
 
 // UPDATES EVERY 20 MILISECONDS

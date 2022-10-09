259e6da535c99622c6c1c1d7cabd0d97be28cc03
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-10-09 17:36:06 -0400

    ball physics still loading..

--- try4/Ball.js
+++ try4/Ball.js
@@ -5,16 +5,21 @@ const LEFT = "left";
 const RIGHT = "right";
 const UP = "up";
 const DOWN = "down";
+const UP_LEFT = "up_left";
+const UP_RIGHT = "up_right";
+const DOWN_LEFT = "down_left";
+const DOWN_RIGHT = "down_right";
 
 export class Ball extends Entity {
   constructor({ getOverlappingPlayer }) {
     super({ className: "ball" });
-    this.SPEED = 4;
+    this.SPEED = 6;
     this.setX(window.innerWidth / 2 - 50);
     this.setY(window.innerHeight - 500);
     this.setDirectionUp();
     this.getOverlappingPlayer = getOverlappingPlayer;
   }
+  // SET DIRECTION STATES
   setDirectionLeft() {
     this.direction = LEFT;
   }
@@ -27,47 +32,93 @@ export class Ball extends Entity {
   setDirectionUp() {
     this.direction = UP;
   }
-  moveDown() {
-    this.setY(this.y + this.SPEED);
-    this.setX(this.x + 1);
+  // 2d directions
+  setDirectionUpLeft() {
+    this.direction = UP_LEFT;
   }
+  setDirectionUpRight() {
+    this.direction = UP_RIGHT;
+  }
+  setDirectionDownLeft() {
+    this.direction = DOWN_LEFT;
+  }
+  setDirectionDownRight() {
+    this.direction = DOWN_RIGHT;
+  }
+
+  // MOVE BALL
+  // TODO : create diagnal movement
   moveUp() {
     this.setY(this.y - this.SPEED);
-    this.setX(this.x + 1);
+  }
+  moveDown() {
+    this.setY(this.y + this.SPEED);
   }
   moveRight() {
     this.setX(this.x + this.SPEED);
-    this.setY(this.y - 1);
   }
   moveLeft() {
     this.setX(this.x - this.SPEED);
-    this.setY(this.y - 1);
+  }
+  moveUpLeft() {
+    this.setY(this.y - this.SPEED);
+    this.setX(this.x - 2);
+  }
+  moveUpRight() {
+    this.setY(this.y - this.SPEED);
+    this.setX(this.x + 2);
+  }
+  moveDownLeft() {
+    this.setY(this.y + this.SPEED);
+    this.setX(this.x - 2);
+  }
+  moveDownRight() {
+    this.setY(this.y + this.SPEED);
+    this.setX(this.x + 2);
   }
 
+  // CHECK DIRECTIONM,
   update() {
     if (this.direction === UP) {
-      this.setY(this.y - this.SPEED);
+      // this.setY(this.y - this.SPEED);
       this.moveUp();
     }
     if (this.direction === DOWN) {
-      this.setY(this.y + this.SPEED);
+      // this.setY(this.y + this.SPEED);
       this.moveDown();
     }
     if (this.direction === RIGHT) {
-      this.setX(this.x + this.SPEED);
+      // this.setX(this.x + this.SPEED);
       this.moveRight();
     }
     if (this.direction === LEFT) {
-      this.setX(this.x - this.SPEED);
+      // this.setX(this.x - this.SPEED);
       this.moveLeft();
+    }
+    if (this.direction === UP_LEFT) {
+      this.moveUpLeft();
+    }
+    if (this.direction === UP_RIGHT) {
+      this.moveUpRight();
+    }
+    if (this.direction === DOWN_LEFT) {
+      this.moveDownLeft();
+    }
+    if (this.direction === DOWN_RIGHT) {
+      this.moveDownRight();
     }
 
     const player = this.getOverlappingPlayer(this);
     if (player) {
       // TODO : configure ball hitting player
-      // console.log(player, this);
-      this.setDirectionUp();
-      this.moveUp();
+
+      if (!(this.x + 30 > player.x + 30) && !(this.x + 30 < player.x)) {
+        this.setDirectionUpLeft();
+      } else if (!(this.x < player.x + 70) && !(this.x > player.x + 100)) {
+        this.setDirectionUpRight();
+      } else {
+        this.setDirectionUp();
+      }
     }
   }
 }
